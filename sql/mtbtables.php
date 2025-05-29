4049d22d58640607bad22b5bcdf6e05c822fb024
derp :3 <133679016+iid3rp@users.noreply.github.com>
2025-05-29 16:54:53 +0800

    updating the database starters...

--- sql/mtbtables.php
+++ sql/mtbtables.php
@@ -12,7 +12,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-
 // sql to create SCHOOL table
 $sqlschool = "CREATE TABLE IF NOT EXISTS school (
     schoolRefNo INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
@@ -29,7 +28,7 @@ $sqlschool = "CREATE TABLE IF NOT EXISTS school (
 )";
 
 if ($conn->query($sqlschool) === TRUE) 
-    echo "Table for Schools created successfully";
+    echo "Table for Schools created successfully" . "<br>";
 else
     echo "Error creating table: " . $conn->error;
 
@@ -44,7 +43,7 @@ $sqlusers = "CREATE TABLE IF NOT EXISTS mtbmalusers (
     emailAddress VARCHAR(50) NOT NULL UNIQUE,
     contactNo VARCHAR(12) NOT NULL UNIQUE,
     username VARCHAR(15) NOT NULL UNIQUE,
-    password VARCHAR(30) NOT NULL,
+    password VARCHAR(60) NOT NULL,
     accCreator INT(10) UNSIGNED NOT NULL,
     accType ENUM ('School Administrator', 'Educator', 'Student'),
     regDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -52,52 +51,55 @@ $sqlusers = "CREATE TABLE IF NOT EXISTS mtbmalusers (
 )";
 
 if ($conn->query($sqlusers) === TRUE) 
-    echo "<br>" . "Table for MTB-MAL Users created successfully.";
+    echo "<br>" . "Table for MTB-MAL Users created successfully." . "<br>";
 else
     echo "Error creating table: " . $conn->error;
 
 
 // sql to create SCHOOL ADMINISTRATOR table
 $sqladmin = "CREATE TABLE IF NOT EXISTS schooladministrator (
-    adminId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    adminNo INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     accRefNo INT(10) UNSIGNED NOT NULL,
     schoolIdNo INT(6) UNSIGNED NOT NULL,
-    fullName VARCHAR(70) NOT NULL,
+    fullName VARCHAR(70) NOT NULL,    
+    adEmpIdNo INT(7) UNSIGNED NOT NULL UNIQUE,
     FOREIGN KEY (accRefNo) REFERENCES mtbmalusers(accRefNo) ON DELETE RESTRICT,
     FOREIGN KEY (schoolIdNo) REFERENCES school(schoolIdNo) ON DELETE CASCADE    
 )";
 
 if ($conn->query($sqladmin) === TRUE) 
-    echo "<br>" . "Table School Administrator created successfully";
+    echo "<br>" . "Table School Administrators created successfully" . "<br>";
 else
     echo "Error creating table: " . $conn->error;
 
 
 // sql to create EDUCATOR table
 $sqleducator = "CREATE TABLE IF NOT EXISTS educator (
-    educatorId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    educatorNo INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     accRefNo INT(10) UNSIGNED NOT NULL,
     schoolIdNo INT(6) UNSIGNED NOT NULL,
     fullName VARCHAR(70) NOT NULL, 
+    edEmpIdNo INT(7) UNSIGNED NOT NULL UNIQUE,
     FOREIGN KEY (accRefNo) REFERENCES mtbmalusers(accRefNo) ON DELETE RESTRICT,
     FOREIGN KEY (schoolIdNo) REFERENCES school(schoolIdNo) ON DELETE CASCADE
 )";
 
 if ($conn->query($sqleducator) === TRUE) 
-    echo "<br>" . "Table for Educators created successfully.";
+    echo "<br>" . "Table for Educators created successfully." . "<br>";
 else
     echo "Error creating table: " . $conn->error;
 
 
 // sql to create STUDENT table
 $sqlstudent = "CREATE TABLE IF NOT EXISTS student (
-    studentId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    studentNo INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     accRefNo INT(10) UNSIGNED NOT NULL,
     schoolIdNo INT(6) UNSIGNED NOT NULL,
+    lrn INT(12) UNSIGNED NOT NULL UNIQUE,
     fullName VARCHAR(70) NOT NULL, 
     parentGuardianName VARCHAR(70) NOT NULL,
     pgRStoStudent ENUM ('Father', 'Mother', 'Guardian', 'Sibling', 'Close Relative'),
-    pgDOB DATE,
+    pgDOB DATE NOT NULL,
     pgMaritalStatus VARCHAR(15) NOT NULL,
     pgEmailAdd VARCHAR(30) NOT NULL, 
     pgContactNo VARCHAR(12) NOT NULL,
@@ -106,7 +108,7 @@ $sqlstudent = "CREATE TABLE IF NOT EXISTS student (
 )";
 
 if ($conn->query($sqlstudent) === TRUE) 
-    echo "<br>" . "Table for Student created successfully.";
+    echo "<br>" . "Table for Student created successfully." . "<br>";
 else
     echo "Error creating table: " . $conn->error;
 
@@ -114,22 +116,234 @@ else
 // sql to create SUBJECT table
 $sqlsubject = "CREATE TABLE IF NOT EXISTS subject (
     subjectRefNo INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    subjectId INT(7) UNSIGNED NOT NULL UNIQUE, 
     subjTitle VARCHAR(155) NOT NULL,
     subjDescription TEXT NOT NULL,
     mtLanguage VARCHAR(30) NOT NULL,
     adminCreator INT(6) UNSIGNED NOT NULL,
-    assignedEducator INT(6) UNSIGNED NULL,
+    assignedEducator INT(10) UNSIGNED NULL,
     schoolIdNo INT(6) UNSIGNED NOT NULL,
-    FOREIGN KEY (adminCreator) REFERENCES schooladministrator(adminId) ON DELETE RESTRICT,
-    FOREIGN KEY (assignedEducator) REFERENCES educator(educatorId) ON DELETE SET NULL,
+    FOREIGN KEY (adminCreator) REFERENCES mtbmalusers(accRefNo) ON DELETE RESTRICT,
+    FOREIGN KEY (assignedEducator) REFERENCES mtbmalusers(accRefNo) ON DELETE SET NULL,
     FOREIGN KEY (schoolIdNo) REFERENCES school(schoolIdNo) ON DELETE CASCADE 
 )";
 
 if ($conn->query($sqlsubject) === TRUE) 
-    echo "<br>" . "Table for Subject created successfully.";
+    echo "<br>" . "Table for Subjects created successfully." . "<br>";
 else
     echo "Error creating table: " . $conn->error;
+
+
+// sql to create LEARNING MATERIAL table "CHAPTER" <learningMaterialRefNo -> successful upload>
+$sqllms = "CREATE TABLE IF NOT EXISTS learningmaterial (
+    learningMaterialRefNo INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    subjectRefNo INT(10) UNSIGNED NOT NULL,
+    chapterNo INT(2) UNSIGNED NOT NULL,
+    title VARCHAR(155) NOT NULL,
+    description TEXT NOT NULL,
+    lmNo INT(3) UNSIGNED NOT NULL,
+    lmType ENUM('Learning Module','Learning Assessment'),
+    diffLevel ENUM ('Easy', 'Intermediate','Hard'),
+    schoolIdNo INT(6) UNSIGNED NOT NULL,
+    sourceRef TEXT NULL,
+    FOREIGN KEY (schoolIdNo) REFERENCES school(schoolIdNo) ON DELETE CASCADE,
+    FOREIGN KEY (subjectRefNo) REFERENCES subject(subjectRefNo) ON DELETE CASCADE
+)";
+
+if ($conn->query($sqllms) === TRUE) 
+    echo "<br>" . "Table for Learning Materials created successfully." . "<br>";
+else
+    echo "Error creating table: " . $conn->error;
+
+
+// sql to create LEARNING MODULE table "LESSON"
+$sqlmodule = "CREATE TABLE IF NOT EXISTS module (
+    moduleId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    lessonNo INT(2) UNSIGNED NOT NULL,
+    learningMaterialRefNo INT(10) UNSIGNED NOT NULL,
+    title VARCHAR(155) NOT NULL,
+    description TEXT NOT NULL,
+    schoolIdNo INT(6) UNSIGNED NOT NULL,
+    FOREIGN KEY (schoolIdNo) REFERENCES school(schoolIdNo) ON DELETE CASCADE,    
+    FOREIGN KEY (learningMaterialRefNo) REFERENCES learningmaterial(learningMaterialRefNo) ON DELETE CASCADE
+)";
+
+if ($conn->query($sqlmodule) === TRUE) 
+    echo "<br>" . "Table for Learning Modules created successfully." . "<br>";
+else
+    echo "Error creating table: " . $conn->error;
+
+
+// sql to create LEARNING ASSESSMENT table "GAMIFIED ASSESSMENTS"
+$sqlassessment = "CREATE TABLE IF NOT EXISTS assessment (
+    assessmentId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    assessmentNo INT(2) UNSIGNED NOT NULL,
+    learningMaterialRefNo INT(10) UNSIGNED NOT NULL,
+    title VARCHAR(155) NOT NULL,
+    description TEXT NOT NULL, 
+    instruction TEXT NULL,
+    scoringRubric TEXT NULL,
+    sample TEXT NULL,
+    schoolIdNo INT(6) UNSIGNED NOT NULL,
+    FOREIGN KEY (schoolIdNo) REFERENCES school(schoolIdNo) ON DELETE CASCADE,  
+    FOREIGN KEY (learningMaterialRefNo) REFERENCES learningmaterial(learningMaterialRefNo) ON DELETE CASCADE
+)";
+
+if ($conn->query($sqlassessment) === TRUE) 
+    echo "<br>" . "Table for Learning Assessments created successfully." . "<br>";
+else
+    echo "Error creating table: " . $conn->error;
+
+
+// sql to create TEMPLATE table <insert data for template tables>
+$sqltemplate = "CREATE TABLE IF NOT EXISTS template (
+    tempRefNo INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    tempName VARCHAR(70) NOT NULL,
+    tempDescription TEXT NOT NULL,
+    tempType ENUM ('Learning Module', 'Learning Assessment'),
+    diffLevel ENUM ('Easy', 'Intermediate','Hard'),
+    learningMaterialRefNo INT(10) UNSIGNED NOT NULL,
+    FOREIGN KEY (learningMaterialRefNo) REFERENCES learningmaterial(learningMaterialRefNo) ON DELETE RESTRICT
+)";
+
+if ($conn->query($sqltemplate) === TRUE) 
+    echo "<br>" . "Table for Templates created successfully." . "<br>";
+else
+    echo "Error creating table: " . $conn->error;
+
+
+// sql to create LEARNING CONTENT table "MODULE TEMPLATE"
+$sqlmodtemp = "CREATE TABLE IF NOT EXISTS learningcontent (
+    modTempNo INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    tempRefNo INT(2) UNSIGNED NOT NULL,
+    topicNo INT(2) NOT NULL, 
+    useby INT(10) UNSIGNED NULL,
+    topic VARCHAR(155) NOT NULL,
+    contentText TEXT NOT NULL,
+    moduleId INT(6) UNSIGNED NOT NULL, 
+    lessonNo INT(2) UNSIGNED NOT NULL, 
+    uploadDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (tempRefNo) REFERENCES template(tempRefNo) ON DELETE RESTRICT,
+    FOREIGN KEY (moduleId) REFERENCES module(moduleId) ON DELETE CASCADE,
+    FOREIGN KEY (useby) REFERENCES mtbmalusers(accRefNo) ON DELETE SET NULL
+)";
+
+if ($conn->query($sqlmodtemp) === TRUE) 
+    echo "<br>" . "Table for Learning Contents created successfully." . "<br>";
+else
+    echo "Error creating table: " . $conn->error;
+
+
+
+// sql to create ASSESSMENT ITEM table "GAME TEMPLATE"
+$sqlitemtemp = "CREATE TABLE IF NOT EXISTS assessmentitems (
+    gameTempNo INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    tempRefNo INT(2) UNSIGNED NOT NULL,    
+    assessmentId INT(6) UNSIGNED NOT NULL,
+    useby INT(10) UNSIGNED NULL,
+    assessmentNo INT(2) UNSIGNED NOT NULL, 
+    itemNo INT(2) NOT NULL,
+    iQuestion TEXT NOT NULL,
+    correctAnswer VARCHAR(255) NOT NULL,
+    uploadDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (tempRefNo) REFERENCES template(tempRefNo) ON DELETE RESTRICT,
+    FOREIGN KEY (assessmentId) REFERENCES assessment(assessmentId) ON DELETE CASCADE,
+    FOREIGN KEY (useby) REFERENCES mtbmalusers(accRefNo) ON DELETE SET NULL
+)";
+
+if ($conn->query($sqlitemtemp) === TRUE) 
+    echo "<br>" . "Table for Assessment Items created successfully." . "<br>";
+else
+    echo "Error creating table: " . $conn->error;
+
+
+
+// sql to create BLOB table "SUPPORTING MEDIA"
+$sqlfile = "CREATE TABLE IF NOT EXISTS supportingmedia (
+    fileNo INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    learningMaterialRefNo INT(10) UNSIGNED NOT NULL,
+    gameTempNo INT(3) UNSIGNED NULL,
+    modTempNo INT(3) UNSIGNED NULL,
+    accRefNo INT(10) UNSIGNED NOT NULL,
+    filename VARCHAR(255) NULL,
+    filedata BLOB NULL,
+    FOREIGN KEY (learningMaterialRefNo) REFERENCES learningmaterial(learningMaterialRefNo) ON DELETE RESTRICT,
+    FOREIGN KEY (gameTempNo) REFERENCES assessmentitems(gameTempNo) ON DELETE SET NULL,
+    FOREIGN KEY (modTempNo) REFERENCES learningcontent(modTempNo) ON DELETE SET NULL,
+    FOREIGN KEY (accRefNo) REFERENCES mtbmalusers(accRefNo) ON DELETE RESTRICT
+)";
+
+if ($conn->query($sqlfile) === TRUE) 
+    echo "<br>" . "Table for Supporting Media created successfully." . "<br>";
+else
+    echo "Error creating table: " . $conn->error;
+
+
+// sql to create ENROLLED STUDENTS table 
+$sqlenroll = "CREATE TABLE IF NOT EXISTS enrolled_students (
+    enrollmentNo INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    subjectRefNo INT(10) UNSIGNED NOT NULL,
+    assignedEducator INT(10) UNSIGNED NOT NULL,
+    studentAccRefNo INT(10) UNSIGNED NOT NULL,
+    status ENUM('Enrolled', 'Removed'),
+    FOREIGN KEY (subjectRefNo) REFERENCES subject(subjectRefNo) ON DELETE CASCADE,
+    FOREIGN KEY (assignedEducator) REFERENCES mtbmalusers(accRefNo) ON DELETE RESTRICT,
+    FOREIGN KEY (studentAccRefNo) REFERENCES mtbmalusers(accRefNo) ON DELETE RESTRICT
+)";
+
+if ($conn->query($sqlenroll) === TRUE) 
+    echo "<br>" . "Table for Enrolled Students created successfully." . "<br>";
+else
+    echo "Error creating table: " . $conn->error;
+
+
+// sql to create STUDENT ASSESSMENT ANSWERS table 
+$sqlenroll = "CREATE TABLE IF NOT EXISTS student_assessment_answers (
+    answerId INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    stLRN INT(12) UNSIGNED NOT NULL,
+    assessmentId INT(6) UNSIGNED NOT NULL,
+    assessmentItemId INT(3) UNSIGNED NOT NULL,
+    studentAnswer VARCHAR(255) NOT NULL,
+    isCorrect BOOLEAN DEFAULT NULL,
+    reviewedBy INT(10) UNSIGNED NULL,
+    reviewDateTime TIMESTAMP NULL DEFAULT NULL,
+    remarks TEXT NULL,
+    FOREIGN KEY (stLRN) REFERENCES student(lrn) ON DELETE CASCADE,
+    FOREIGN KEY (assessmentId) REFERENCES assessment(assessmentId) ON DELETE CASCADE,
+    FOREIGN KEY (assessmentItemId) REFERENCES assessmentitems(gameTempNo) ON DELETE CASCADE,
+    FOREIGN KEY (reviewedBy) REFERENCES mtbmalusers(accRefNo) ON DELETE SET NULL
+)";
+
+if ($conn->query($sqlenroll) === TRUE) 
+    echo "<br>" . "Table for Student Assessment Answers created successfully." . "<br>";
+else
+    echo "Error creating table: " . $conn->error;
+
+
+// sql to create STUDENT ASSESSMENT RESULTS table 
+$sqlenroll = "CREATE TABLE IF NOT EXISTS student_assessment_result (
+    resultId INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    stLRN INT(12) UNSIGNED NOT NULL,
+    assessmentId INT(6) UNSIGNED NOT NULL,
+    totalItems INT UNSIGNED NOT NULL,
+    correctAnswers INT UNSIGNED NOT NULL,
+    rawScore DECIMAL(5,2) GENERATED ALWAYS AS (correctAnswers / totalItems * 100) STORED,
+    remarks TEXT NULL,
+    finalizedBy INT(10) UNSIGNED NULL,
+    finalDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (stLRN) REFERENCES student(lrn) ON DELETE CASCADE,
+    FOREIGN KEY (assessmentId) REFERENCES assessment(assessmentId) ON DELETE CASCADE,
+    FOREIGN KEY (finalizedBy) REFERENCES mtbmalusers(accRefNo) ON DELETE SET NULL
+)";
+
+if ($conn->query($sqlenroll) === TRUE) 
+    echo "<br>" . "Table for Student Assessment Results created successfully." . "<br>";
+else
+    echo "Error creating table: " . $conn->error;
+
 
 // Close the database connection
 $conn->close();
 ?>
+
+
